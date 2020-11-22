7bb82d51bdcdb3ece6659f75bcb8577e7a4af8ee
arapinosuke <arashi8221@gmail.com>
2020-11-22 16:31:25 +0900

    cards

---
+++ cards2/Assets/Scripts/Card.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card
+{
+    public enum Suit
+    {
+        Invalid = -1,
+        Club,
+        Dia,
+        Heart,
+        Spade,
+        Max
+    };
+
+    public Suit CardSuit = Suit.Invalid;
+
+    public int Number = 0;
+
+    public int Num = 0;
+
+    public const int CardMax = 52;
+
+    public Card(Suit suit, int number, int num)
+    {
+        CardSuit = suit;
+        Number = number;
+        Num = num;
+    }
+    
+    public static int CardNumJudge(int _num)
+    {
+        for (int i = 0; i < 13; i++)
+        {
+            if (_num % 13 == i)
+            {
+                return i + 1;
+            }
+        }
+        return 0;
+    }
+
+    public static Suit CardSuitJudge(int _num)
+    {
+        for (int i = 0; i < (int)Suit.Max; i++)
+        {
+            if (_num / 13 == i)
+            {
+                return (Suit)i;
+            }
+        }
+        return Suit.Invalid;
+    }
+}
---
+++ cards2/Assets/Scripts/Deck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Deck
+{
+    public static List<Card> GetDeck()
+    {
+        var deck = new List<Card>();
+
+        for (int i = 0; i < Card.CardMax; i++)
+        {
+            deck.Add(new Card(Card.CardSuitJudge(i), Card.CardNumJudge(i), i));
+        }
+        return deck;
+    }
+
+    public static List<Card> ShuffleDeck(List<Card> deck)
+    {
+        var shuffleDeck = deck.OrderBy(card => Guid.NewGuid()).ToList();
+        return shuffleDeck;
+    }
+
+    public static Card GetCard(List<Card> deck)
+    {
+        var card = deck.First();
+        deck.RemoveAt(0);
+        return card;
+    }
+}
