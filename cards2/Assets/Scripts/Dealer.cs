b57e0a6863ca684360250183ad95ff084f2eb7fd
arapinosuke <arashi8221@gmail.com>
2020-12-06 16:19:16 +0900

    勝ち負け表示

---
+++ cards2/Assets/Scripts/Dealer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Dealer : MonoBehaviour
+{
+    private List<Card> GameDeck = new List<Card>();
+
+    private void Awake()
+    {
+        GameStart();
+    }
+
+    public List<Card> GetPlayerDeck()
+    {
+        var playerDeck = GameDeck.Where((c, i) => i % 2 != 0).ToList();
+        return playerDeck;
+    }
+
+    public List<Card> GetCPUDeck()
+    {
+        var cpuDeck = GameDeck.Where((c, i) => i % 2 == 0).ToList();
+        return cpuDeck;
+    }
+
+    public void GameStart()
+    {
+        GameDeck = Deck.GetDeck();
+        GameDeck = Deck.ShuffleDeck(GameDeck);
+    }
+}
---
+++ cards2/Assets/Scripts/HighAndLowSequence.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighAndLowSequence : MonoBehaviour
+{
+   private enum GameSequece
+    {
+        Invalide,
+        Init,
+        Start,
+        Deal,
+        PlayerJudge,
+        show
+    }
+
+    private GameSequece gameSequece = GameSequece.Invalide;
+
+    public Dealer dealer;
+
+    public PlayerCard playerCard;
+
+    public CPUCard cpuCard;
+
+    public PlayerJudge playerJudge;
+
+    public bool GameOver = false;
+   
+
+     void Update()
+    {
+        switch (gameSequece)
+        {
+            case GameSequece.Invalide:
+
+                gameSequece = GameSequece.Init;
+                break;
+
+            case GameSequece.Init:
+
+                playerCard.SetPlayerDeck();
+                cpuCard.SetCPUDeck();
+                gameSequece = GameSequece.Deal;
+                break;
+
+            case GameSequece.Start:
+
+                gameSequece = GameSequece.Deal;
+                break;
+
+            case GameSequece.Deal:
+
+                playerCard.SetPlayerCard();
+                cpuCard.SetCPUCard();
+
+                Debug.Log(playerCard.playerCard.Number);
+                Debug.Log(cpuCard.cpuCard.Number);
+                gameSequece = GameSequece.PlayerJudge;
+                break;
+            case GameSequece.PlayerJudge:
+
+                if (playerJudge.Judge)
+                {
+                    gameSequece = GameSequece.show;
+                }
+                break;
+
+            case GameSequece.show:
+
+                cpuCard.ShowCPUCard();
+
+                if (playerJudge.High)
+                {
+                    if (playerCard.playerCard.Number>cpuCard.cpuCard.Number)
+                    {
+                        Debug.Log("勝ち");
+                    }
+                    else
+                    {
+                        Debug.Log("負け");
+                    }
+                }
+                else
+                {
+                    if (playerCard.playerCard.Number < cpuCard.cpuCard.Number)
+                    {
+                        GameOver = false;
+                        Debug.Log("勝ち");
+                    }
+                    else
+                    {
+                        Debug.Log("負け");
+                    }
+                }
+                playerJudge.Judge = false;
+                gameSequece = GameSequece.Start;
+                break;
+        }
+    }
+}
